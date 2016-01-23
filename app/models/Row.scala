8ae5124162fe5ee6dc4a34585ef954fb0ccfb206
Sergii Khomenko <sergii.khomenko@stylight.com>
2016-01-23 20:42:47 +0100

    added support for Nullable String and BigDecimal

--- app/models/Row.scala
+++ app/models/Row.scala
@@ -54,8 +54,8 @@ class Row {
       case a: Timestamp => Json.toJson(a)
       case a: java.sql.Time => Json.toJson(a)
       case a: Date => Json.toJson(a)
-      case a: java.math.BigInteger => Json.toJson( a.toString()) //TEMPORARY SOLUTION WRITE
-      case a: java.math.BigDecimal => Json.toJson(JsNumber(a))
+      case a: java.math.BigInteger => Json.toJson(a.toString()) //TEMPORARY SOLUTION WRITE
+      case a: BigDecimal => Json.toJson(a)
 
       case _ => throw new RuntimeException("Type not serializable : "+a)
     }
